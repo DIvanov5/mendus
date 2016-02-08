3ec1214be398d5410d1753681a5c4cffe463c281
Dennis Ivanov <densky588@gmail.com>
2016-02-08 22:02:12 +0200

    new files

--- js/common.js
+++ js/common.js
@@ -1,5 +1,31 @@
+// Height for bg image
 $(document).ready(function() {
+	function heightDetect(){
+	$(".main_head").css("height", $(window).height());	
+	};
+	heightDetect()
+	$(window).resize(function(){
+		heightDetect();
+	});
 
-	
-	
+});
+
+// Preload function
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
+
+// Menu
+$(".toggle_mnu, .menu_item").click(function() {
+  $(".sandwich").toggleClass("active");
+});
+
+$(".toggle_mnu").click(function() {
+	if ($(".top_mnu").is(":visible")) {
+  	$(".top_mnu").fadeOut(600);
+  }
+  else {
+  	$(".top_mnu").fadeIn(600);
+  };
 });
