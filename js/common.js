7dd9503b1d8332a6d19cbcf2167f1c21a1021449
Den Ivanov <densky588@gmail.com>
2016-02-12 19:26:39 +0200

    seventh commit

--- js/common.js
+++ js/common.js
@@ -1,6 +1,11 @@
 // Height for bg image
 
 $(document).ready(function() {
+
+// Main title animation
+	$(".h_title h1, svg path").animated("fadeInDown", "fadeOutUp");
+	$(".section_header").animated("fadeInUp", "fadeOutDown");
+
 	function heightDetect(){
 	$(".main_head").css("height", $(window).height());	
 	};
@@ -9,38 +14,37 @@ $(document).ready(function() {
 		heightDetect();
 	});
 
-});
+	});
 
 // Preload function
 
-$(window).load(function() { 
-	$(".loader_inner").fadeOut(); 
-	$(".loader").delay(400).fadeOut("slow"); 
-});
+	$(window).load(function() { 
+		$(".loader_inner").fadeOut(); 
+		$(".loader").delay(400).fadeOut("slow"); 
+	});
 
 // Menu
 
-$(".toggle_mnu").click(function() {
-  $(".sandwich").toggleClass("active");
-});
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
 
 //Hide menu when click on menu item
 
-$(".top_mnu ul a").click(function() {
-	$(".top_mnu").fadeOut(600);
-	$(".sandwich").toggleClass("active");
-}).append("<span>");
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	}).append("<span>");
 
-$(".toggle_mnu").click(function() {
-	if ($(".top_mnu").is(":visible")) {
-		$(".top_text").removeClass("h-opacify"); //On menu click, change text opacity
-  	$(".top_mnu").fadeOut(600); // Show menu
-  	$(".top_mnu li a").removeClass("fadeInUp animated");
-  }
-
-  else {
-  	$(".top_text").addClass("h-opacify");
-  	$(".top_mnu").fadeIn(600);
-  	$(".top_mnu li a").addClass("fadeInUp animated");
-  };
-});
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".h_title, svg").removeClass("h-opacify"); //On menu click, change text opacity
+			$(".top_mnu").fadeOut(600); // Show menu
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+	}
+		else {
+			$(".h_title, svg").addClass("h-opacify");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
